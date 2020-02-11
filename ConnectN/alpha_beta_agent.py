3737c0fc6905e0c20e592bcdd8594ed1b2f98a37
Dontpuntme <david.martindale.99@gmail.com>
2020-02-11 16:15:21 -0500

    Brain Power Increasing
    Still weird bugs though

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -25,7 +25,7 @@ class AlphaBetaAgent(agent.Agent):
     # NOTE: make sure the column is legal, or you'll lose the game.
     def go(self, brd):
         """Search for the best move (choice of column for the token)"""
-        return self.getMove(brd,3,self.max_depth,True,-1000000000,1000000000)[1]
+        return self.getMove(brd,3,self.max_depth,True,-1000000000,1000000000,self.what_player(brd))[1]
         
     # Get the successors of the given board.
     # PARAM [board.Board] brd: the board state
@@ -50,28 +50,51 @@ class AlphaBetaAgent(agent.Agent):
             # Add board to list of successors
             succ.append((nb,col))
         return succ
+    def what_player(self,brd):
+        total = 0
+        for x in range(brd.w):
+            for y in range(brd.h):
+                if (brd.board[y][x] == 1):
+                    total +=1
+                elif (brd.board[y][x] == 2):
+                    total -=1
+        if total==1:
+            return 2
+        else:
+            return 1
+        
+                    
 
 
-    def getMove(self, brd, col, depth, isMaximizingPlayer, alpha, beta):
+    def getMove(self, brd, col, depth, isMaximizingPlayer, alpha, beta,player):
         if depth == 0:
 # =============================================================================
 #             print("TESTING COLUMN: ", col)
 #             brd.print_it()
 #             print("SCORE OF THIS BOARD IS", self.getValue(brd))
 # =============================================================================
-            return (brd, col, self.getValue(brd))
+            return (brd, col, self.getValue(brd,player))
         elif isMaximizingPlayer:
             bestVal = (brd,col, -1000000000)
             bestValList=list(bestVal)
             for node in self.get_successors(brd):
-                #                   board    col     depth     IsMaximizing     
-                MoveTuple=self.getMove(node[0], node[1], depth-1, False, alpha, beta)
+# =============================================================================
+#                 if node[0].get_outcome==2:
+#                     bestValList[0]=node[0]
+#                     bestValList[1]=node[1]
+#                     bestValList[2]=1000       
+#                     return bestValList
+# =============================================================================
+                #                   board    col     depth     IsMaximizing
+                MoveTuple=self.getMove(node[0], node[1], depth-1, False, alpha, beta,player)
                 if MoveTuple[2]>bestValList[2]:
                     bestValList[0]=node[0]
                     bestValList[1]=node[1]
                     bestValList[2]=MoveTuple[2]        
-                    print("called")
-                    print(bestValList[2])
+# =============================================================================
+#                     print("called")
+#                     print(bestValList[2])
+# =============================================================================
                 if bestValList[2]>=beta :
                     return bestValList
                 alpha = max(alpha, bestValList[2])
@@ -82,7 +105,14 @@ class AlphaBetaAgent(agent.Agent):
             bestVal = (brd,col, 1000000000)
             bestValList=list(bestVal)
             for node in self.get_successors(brd):
-                MoveTuple=self.getMove(node[0], node[1], depth-1, True, alpha, beta)
+# =============================================================================
+#                 if node[0].get_outcome==1:
+#                     bestValList[0]=node[0]
+#                     bestValList[1]=node[1]
+#                     bestValList[2]=-1000       
+#                     return bestValList
+# =============================================================================
+                MoveTuple=self.getMove(node[0], node[1], depth-1, True, alpha, beta,player)
                 if MoveTuple[2]<bestValList[2]:
                     bestValList[0]=node[0]
                     bestValList[1]=node[1]
@@ -94,13 +124,54 @@ class AlphaBetaAgent(agent.Agent):
             return bestValList
         
         
-    def getValue(self, brd):
+    def getValue(self,brd,player):
         if brd.get_outcome() == 2:
             return 1000
         if brd.get_outcome() == 1:
             return -1000
-        if brd.get_outcome() == 0:
+        else:
+           return self.num_threes(brd,player)
+       
+    def is_three_at(self,brd, x, y, dx, dy, player):
+        """Return True if a line of identical tokens exists starting at (x,y) in direction (dx,dy)"""
+        # Avoid out-of-bounds errors
+        if ((x + (4-1) * dx >= brd.w) or
+            (y + (4-1) * dy < 0) or (y + (4-1) * dy >= brd.h)):
             return 0
+        # Get token at (x,y)
+        t = brd.board[y][x]
+        # Go through elements
+        if t == 0:
+            return 0
+        total = 0
+        for i in range(1, 4):
+            if brd.board[y + i*dy][x + i*dx] != t and brd.board[y + i*dy][x + i*dx] != 0:
+                total += 1
+        if total>1 and player==t:
+            return 1
+        elif total>1 and player!=t:
+            return -1
+        else:
+            return 0
+    def num_threes_at(self,brd,x, y,player):
+        """Return True if a line of identical tokens exists starting at (x,y) in any direction"""
+        return (self.is_three_at(brd,x, y, 1, 0, player) + # Horizontal
+                self.is_three_at(brd,x, y, 0, 1, player) + # Vertical
+                self.is_three_at(brd,x, y, 1, 1, player) + # Diagonal up
+                self.is_three_at(brd,x, y, 1, -1,player)) # Diagonal down
+    def num_threes(self,brd,player):
+        """Returns the winner of the game: 1 for Player 1, 2 for Player 2, and 0 for no winner"""
+        total = 0
+        for x in range(brd.w):
+            for y in range(brd.h):
+                if (brd.board[y][x] != 0):
+                    total+=self.num_threes_at(brd,x,y,player)
+        return total
+
+        
+# =============================================================================
+#         return brd.numThrees
+# =============================================================================
 #Thomas Alpha Beta and 3 in a row counting.
 #David most sets of N
 
